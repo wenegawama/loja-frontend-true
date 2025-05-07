73a6b919ca9d2fcad45c2a92c586c4d951f9b85b
wenegawama <68117130+wenegawama@users.noreply.github.com>
2025-05-07 15:41:24 -0300

    h8 quase finalizado

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,2 +1,62 @@
+/*
+let contador  = 1
 
+document.getElementById('btnMais').addEventListener('click', function aumentar() {
+    const contador = document.getElementById('inputQuantidade').value
+    //input.value = quantidade
+    console.log(contador)
+}) 
 
+function aumentar(contador = 1) {
+    return contador ++
+}
+
+let productData = document.cookie
+console.log('Carrinho - Dados do item produto adicionado : '+ productData)
+console.log('Tamanho do carrinho: '+ productData.length)
+
+*/
+
+/*--------------------------------------*/
+function mostrarCarrinho () {
+    const cart = JSON.parse(localStorage.getItem('carrinho')) || []
+    console.log(cart)
+    const tabela = document.getElementById('tabela')
+    tabela.innerHTML = ''
+
+    cart.forEach((item, index) => {
+        const linha = document.createElement('tr')
+        linha.innerHTML = 
+            `
+                <th scope="row">${index + 1}</th>
+                    <td>${item.name}</td>
+                    <td>${item.price}</td>
+                    <td>
+                    <div class="container align-items-center">
+                        <button class="botao" id="btnMenos"><img src="/images/menos.png" alt="Botão menos" height="20px" width="20px"></button>
+                        <input type="text" name="" id="inputQuantidade" class="inputQuantidade" height="20px"  value="1">
+                        <button class="botao" id="btnMais"><img src="/images/mais.png" alt="Botão mais" height="20px" width="20px"></button>
+                    </div>
+                    </td>
+                    <td><button class="text-white bg-danger  rounded" onclick="removerDoCarrinho(${index})">Remover</button></td>
+            `
+            tabela.appendChild(linha)
+    })
+    atualizarCarrinho(cart)
+}
+
+function atualizarCarrinho (cart) {
+    const total = cart.reduce((sum, item) => sum + parseFloat(item.price), 0) 
+
+    document.getElementById('total').innerHTML = `<p id="total" px-5><b>Total : </b>R$ ${total.toFixed(2)} </p>`
+}
+
+function removerDoCarrinho (index) {
+    const cart = JSON.parse(localStorage.getItem('carrinho')) || []
+    cart.splice(index, 1)
+
+    localStorage.setItem('carrinho', JSON.stringify(cart))
+    mostrarCarrinho()
+}
+
+document.addEventListener ('DOMContentLoaded', mostrarCarrinho) 
--- js/produto.js
+++ js/produto.js
@@ -1,43 +1,104 @@
 function getProductIdFromUrl() {
-    const params = new URLSearchParams(window.location.search)
-    console.log(params)
-    return params.get('id')
+  const params = new URLSearchParams(window.location.search);
+  console.log(params);
+  return params.get("id");
 }
 
 async function fetchSingleProduct(id) {
-    const urlBase =  `http://NPRCURJBE02PYDW.REDECORP.BR:8080/api/v1/users/sellers/produtos/buscar?id=${id}`
-    
-    const response = await fetch(urlBase)
-    const product = await response.json()
+  const urlBase = `http://NPRCURJBE02PYDW.REDECORP.BR:8080/api/v1/users/sellers/produtos/buscar?id=${id}`;
+  const response = await fetch(urlBase);
+  const product = await response.json();
 
-    //console.log(product)
+  try {
+    document.getElementById("name").textContent = product.name;
+    document.getElementById("price").textContent = product.price;
+    document.getElementById("tamanho").textContent = product.tamanho;
+    document.getElementById("category").textContent = product.category;
+    document.getElementById("description").textContent = product.description;
 
-    try {
-        document.getElementById('name').textContent = product.name
-        document.getElementById('price').textContent = product.price
-        document.getElementById('tamanho').textContent = product.tamanho
-        document.getElementById('category').textContent = product.category
-        document.getElementById('description').textContent = product.description
+    const itemProduct = {
+      name: document.getElementById("name").textContent,
+      price: document.getElementById("price").textContent,
+      tamanho: document.getElementById("tamanho").textContent,
+      category: document.getElementById("category").textContent,
+      description: document.getElementById("description").textContent,
+    };
 
-    } catch(error) {
-        console.log(error)
-    }
+    //saveProductToCookie(itemProduct);
+
+    /*let products = [];
+        const productsCookie = getCookie('products');
+        if (productsCookie) {
+            products = JSON.parse(productsCookie);
+        }
+
+        products.push(itemProduct);
+        document.cookie = `products=${JSON.stringify(products)}; path=/; max-age=86400`;
+        console.log('Tamanho da lista dos produtos: ' + products.length);
+        */
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const id = getProductIdFromUrl()
-    if(id) {
-        fetchSingleProduct(id)
-    } else {
-        console.log('Id do produto não encontrado')
-    }
-})
+/*
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
 
-document.getElementById('addToCartButton').addEventListener('click', function() {
-    var successModal = new bootstrap.Modal(document.getElementById('successModal'));
+function saveProductToCookie(itemProduct) {
+    let products = [];
+    const productsCookie = getCookie('products');
+    if (productsCookie) {
+        products = JSON.parse(productsCookie);
+    }
+
+    products.push(itemProduct);
+    document.cookie = `products=${JSON.stringify(products)}; path=/; max-age=86400`;
+    console.log('Produto salvo no cookie: ', itemProduct);
+    console.log('Tamanho da lista dos produtos: ' + products.length)
+}
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const id = getProductIdFromUrl();
+  if (id) {
+    fetchSingleProduct(id);
+  } else {
+    console.log("Id do produto não encontrado");
+  }
+});
+
+document
+  .getElementById("addToCartButton")
+  .addEventListener("click", function () {
+    var successModal = new bootstrap.Modal(
+      document.getElementById("successModal")
+    );
     successModal.show();
 
-    setTimeout(function() {
-        successModal.hide();
-      }, 3000);
+    setTimeout(function () {
+      successModal.hide();
+    }, 2000);
   });
+
+function salvarProdutoCarrinho(produto) {
+  const carrinho = JSON.parse(localStorage.carrinho || "[]");
+  carrinho.push(produto);
+  localStorage.carrinho = JSON.stringify(carrinho);
+  console.log("Carrinho" + carrinho);
+}
+
+document.getElementById("addToCartButton").addEventListener("click", () => {
+  const produto = {
+    name: document.getElementById("name").textContent,
+    price: document.getElementById("price").textContent,
+    tamanho: document.getElementById("tamanho").textContent,
+    category: document.getElementById("category").textContent,
+    description: document.getElementById("description").textContent,
+  };
+
+  salvarProdutoCarrinho(produto);
+});
