83b838546ac1f7bef57cf93600459d6c7edcbd65
wenegawama <68117130+wenegawama@users.noreply.github.com>
2025-07-19 19:09:27 -0300

    massive upload products

--- js/index.js
+++ js/index.js
@@ -1,49 +1,51 @@
-let products = []
+let products = [];
 
-async function fetchAllProducts () {
-            const urlBase = "http://NPRCURJBE02PYDW.REDECORP.BR:8080/api/v1/users/sellers/products"
-            fetch(urlBase)
-            .then(response => response.json())
-            .then(response => {
-                products = response
-                displayProducts(products)
-                searchProducts()
-    })
-            //console.log(products)
+async function fetchAllProducts() {
+  const urlBase =
+    "http://NPRCURJBE02PYDW.REDECORP.BR:8080/api/v1/users/sellers/products";
+  fetch(urlBase)
+    .then((response) => response.json())
+    .then((response) => {
+      products = response;
+      displayProducts(products);
+      searchProducts();
+    });
+  //console.log(products)
 }
 
 function displayProducts(products) {
-    const productContainer = document.getElementById('productList')
-    
-    let produtos = ''
-    products.forEach(product => {
+  const productContainer = document.getElementById("productList");
 
-        produtos = produtos+ 
-        `<div class="col-md-4 mb-4">
-            <div class="card">
-            <a href="/pages/produto.html?id=${product.id}">
-                <img
-                src="https://www.chersamis.com.br/media/img/content/produtos/default.jpg"
-                width="20px" class="card-img-top" alt="${product.name}">
-                <div class="card-body">
+  let produtos = "";
+  products.forEach((product) => {
+    produtos =
+      produtos +
+      `<div class="container my-4">
+        <div class="d-flex flex-wrap justify-content-start">
+          <div class="col-md-4  product-card">
+            <div class="card h-100 text-center shadow-sm">
+              <img src="https://images.tcdn.com.br/img/img_prod/809258/camiseta_preta_malha_pv_manga_curta_gola_redonda_267_1_948ec668619667388dfc700bf588df6d.jpg">
                 <h5 class="card-title">${product.name}</h5>
-                <p>R$ ${product.price}</p>
-                </div>
-            </a>
+                <p class="card-text text-danger fs-5">R$ ${product.price}</p>
+                <a href="#" class="btn btn-danger mt-auto">Comprar</a>
             </div>
-      </div>`        
-    });
-    
-    productContainer.innerHTML = produtos;
+          </div>
+        </div>
+      </div>`;
+  });
+
+  productContainer.innerHTML = produtos;
 }
 
 function searchProducts() {
-    const searchInput = document.getElementById('searchInput')
-    searchInput.addEventListener('keyup', function () {
-        const input = this.value.toLowerCase()
-        const filterProducts = products.filter(product => product.name.toLowerCase().includes(input))
-        displayProducts(filterProducts)
-    })
+  const searchInput = document.getElementById("searchInput");
+  searchInput.addEventListener("keyup", function () {
+    const input = this.value.toLowerCase();
+    const filterProducts = products.filter((product) =>
+      product.name.toLowerCase().includes(input)
+    );
+    displayProducts(filterProducts);
+  });
 }
 
 window.onload = () => {
